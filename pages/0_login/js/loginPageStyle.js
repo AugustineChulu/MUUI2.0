6e30da992fcc7fd120974480123b2eee103c2d2c
AugustineChulu <60852841+AugustineChulu@users.noreply.github.com>
2023-10-19 16:20:07 +0200

    compatibility fixes and changes

--- pages/0_login/js/loginPageStyle.js
+++ pages/0_login/js/loginPageStyle.js
@@ -1,3 +1,5 @@
+/* FOCUS AND BLUR STYLES FOR THE INPUT FIELDS ON THE LOGIN FORM */
+
 const unField = document.getElementById('unField');
 const pwField = document.getElementById('pwField');
 
@@ -23,4 +25,28 @@ function removeStyle(){
 	unField.removeAttribute('style');
 	pwField.removeAttribute('style');
 
+}
+
+
+/* CREDENTIALS VERIFICATION FOR THE LOGIN FORM */
+
+document.getElementById('login_form').addEventListener('submit', checkCredentials);
+
+function checkCredentials(event){
+
+	event.preventDefault();
+
+	let username = document.getElementById('unField');
+	let password = document.getElementById('pwField');
+
+	if((username.checkValidity() === true) && (password.checkValidity() === true)){
+
+		if(username.value === 'ICTAZ' && password.value === 'members'){
+			window.location.replace('../1_home/home.html');
+		}else{
+			alert('incorrect credentials entered! try again');
+			window.location.replace('../0_login/login.html');
+		}
+
+	}
 }
