941a5e35c071de70c9ded6a2c62be59a1ab953ee
AugustineChulu <60852841+AugustineChulu@users.noreply.github.com>
2023-11-03 16:05:22 +0200

    collapsible welcome message fixes and optimisation

--- pages/1_home/js/homeStyle.js
+++ pages/1_home/js/homeStyle.js
@@ -16,6 +16,9 @@ function changeVisibility()
 	if (welcomeMsgContainerStyle.getPropertyValue('display') === 'block'){
 
 		welcomeMsgContainer.setAttribute('style', 'display: none;');
+
+		collapsibleHandle.setAttribute('style', 'border-bottom-right-radius: var(--corner_radius); \
+												 border-bottom-left-radius: var(--corner_radius);');
 		
 		collapsibleHandleArrow.style.transform = 'rotate(180deg)';
 
@@ -24,6 +27,8 @@ function changeVisibility()
 
     	welcomeMsgContainer.setAttribute('style', 'display: block;');
     	
+		collapsibleHandle.setAttribute('style', 'border-radius: none;');
+
     	collapsibleHandleArrow.style.transform = 'rotate(0deg)';
 
 	}
